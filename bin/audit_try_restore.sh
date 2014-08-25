3a11af1c414f30bd8b3ce248bcd4fd3c00be562f
Nate Fitch <nate.fitch@joyent.com>
2014-08-25 18:26:12 +0000

    TOOLS-607: I for one, welcome my new open source license

--- bin/audit_try_restore.sh
+++ bin/audit_try_restore.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 #
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+#
+# Copyright (c) 2014, Joyent, Inc.
+#
+
+#
 # This script will sort through audit output and attempt to restore objects from
 # tombstone back to the proper place.  It's a bit brute-force so it could take
 # a while.
